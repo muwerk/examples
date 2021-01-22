6320adb1250116d4335f7f3cc56eb08c2419bd3c
Dominik Schlösser <dsc@dosc.net>
2021-01-22 11:24:46 +0100

    more platforms

--- muBlink/src/led.cpp
+++ muBlink/src/led.cpp
@@ -1,45 +1,72 @@
+#define __UNO__ 1
+#undef __APPLE__
+
 #include "platform.h"
 #include "scheduler.h"
+
+#ifdef USD_FEATURE_NET
 #include "net.h"
 #include "mqtt.h"
 #include "ota.h"
+#endif
 
+#if USTD_FEATURE_MEMORY >= USTD_FEATURE_MEM_8K
 #include "console.h"
+#endif
+
 #include "led.h"
 //#include "switch.h"
 
 void appLoop();
 
+// XXX: better default in muwerk itself!
+#ifdef __UNO__
+ustd::Scheduler sched(2, 2, 2);
+#else
 ustd::Scheduler sched(10, 16, 32);
+#endif
+
+#if USTD_FEATURE_MEMORY >= USTD_FEATURE_MEM_8K
 ustd::SerialConsole con;
+#endif
+
+#ifdef USTD_FEATURE_NET
 ustd::Net net(LED_BUILTIN);
 ustd::Mqtt mqtt;
 ustd::Ota ota;
+#endif
 
+#if defined(__ESP__) && !defined(__ESP32__)
 ustd::Led led1("myLed1", D5, false);
 ustd::Led led2("myLed2", D6, false);
-ustd::Led led3("myLed3", D7, false);
+#endif
+
+#ifdef __ESP32__
+ustd::Led led1("myLed1", 12, false);
+ustd::Led led2("myLed2", 27, false);
+#endif
+
+#ifdef __UNO__
+ustd::Led led1("myLed1", 3, false);
+ustd::Led led2("myLed2", 5, false);
+#endif
 
 void setup() {
-#ifdef USE_SERIAL_DBG
+#if USTD_FEATURE_MEMORY >= USTD_FEATURE_MEM_8K
     Serial.begin(115200);
-#endif  // USE_SERIAL_DBG
     con.begin(&sched);
+#endif
+#ifdef USTD_FEATURE_NET
     net.begin(&sched);
     mqtt.begin(&sched);
     ota.begin(&sched);
+#endif
     /*int tID = */ sched.add(appLoop, "main", 1000000);
     led1.begin(&sched);
     led2.begin(&sched);
-    led3.begin(&sched);
 
-    // generate software phase
     led1.setMode(ustd::Led::Mode::Wave, 2000, 0.0);
-    led1.setMinMaxWaveBrightness(0.02, 0.3);
     led2.setMode(ustd::Led::Mode::Wave, 2000, 0.5);
-    led2.setMinMaxWaveBrightness(0.02, 0.3);
-    led3.setMode(ustd::Led::Mode::Wave, 500, 0.0);
-    led3.setMinMaxWaveBrightness(0.02, 0.3);
 }
 
 void appLoop() {

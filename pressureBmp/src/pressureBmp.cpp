f85795bbc60ef2f46979362633329bf43216bdb7
Dominik Schlösser <dsc@dosc.net>
2022-07-11 16:46:48 +0200

    Multi-sensor tests

--- pressureBmp/src/pressureBmp.cpp
+++ pressureBmp/src/pressureBmp.cpp
@@ -6,6 +6,8 @@
 
 #include "console.h"
 #include "mup_presstemp_bmp180.h"
+#include "mup_illuminance_ldr.h"
+#include "mup_temphum_dht.h"
 #include "mup_oled.h"
 
 void appLoop();
@@ -16,14 +18,16 @@ ustd::Net net(LED_BUILTIN);
 ustd::Mqtt mqtt;
 ustd::Ota ota;
 
-ustd::PressTempBMP180 bmp("myBMP180", ustd::PressTempBMP180::FilterMode::FAST);
+ustd::PressTempBMP180 bmp("BMP180-1", ustd::PressTempBMP180::FilterMode::FAST);
+ustd::IlluminanceLdr ldr("Ldr-1", A0);
+ustd::TempHumDHT dht("DHT-1", D5, 0);
 ustd::SensorDisplay display(128,64,0x3c);
 
 void setup() {
 #ifdef USE_SERIAL_DBG
     Serial.begin(115200);
 #endif  // USE_SERIAL_DBG
-    display.begin("myBMP180/sensor/temperature", "hastates/sensor/balkon_temperature/state", "myBMP180/sensor/pressureNN", "Studio C", "Balkon C", "Luftdruck hPa");
+    display.begin("DHT-1/sensor/temperature", "hastates/sensor/balkon_temperature/state", "BMP180-1/sensor/pressureNN", "Studio C", "Balkon C", "Luftdruck hPa");
     display.updateDisplay("init","init","init",0.0,0.0,0.0);
 
     con.begin(&sched);
@@ -37,13 +41,16 @@ void setup() {
     bmp.startRelativeAltitude(); // Use next pressureNN measurement as altitude reference
     bmp.begin(&sched, ustd::PressTempBMP180::BMPSampleMode::ULTRA_HIGH_RESOLUTION);
 
+    ldr.begin(&sched);
+    dht.begin(&sched);
+
     // subscribe to kernel's MQTT messages, the sensorUpdates() funktion does the event handling
     // Interal BMP180 sensor
     auto fnall = [=](String topic, String msg, String originator) {
             display.sensorUpdates(topic, msg, originator);
         };
-    sched.subscribe(tID, "myBMP180/sensor/temperature", fnall);
-    sched.subscribe(tID, "myBMP180/sensor/pressureNN", fnall);
+    sched.subscribe(tID, "DHT-1/sensor/temperature", fnall);
+    sched.subscribe(tID, "BMP180-1/sensor/pressureNN", fnall);
     // Get outside temp via homeassistant mqtt:
     mqtt.addSubscription(tID,"hastates/sensor/balkon_temperature/state",fnall);
 }

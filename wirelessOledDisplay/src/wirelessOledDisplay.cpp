ef14b53c8a4f20588b0b526b5aaaca4318de3ed2
Dominik Schlösser <dsc@dosc.net>
2022-07-26 21:29:07 +0200

    Universal MQTT display

--- wirelessOledDisplay/src/wirelessOledDisplay.cpp
+++ wirelessOledDisplay/src/wirelessOledDisplay.cpp
@@ -3,11 +3,9 @@
 #include "net.h"
 #include "mqtt.h"
 #include "ota.h"
+#include "jsonfile.h"
 
 #include "console.h"
-#include "mup_presstemp_bmp180.h"
-#include "mup_illuminance_ldr.h"
-#include "mup_temphum_dht.h"
 #include "mup_oled.h"
 
 void appLoop();
@@ -17,18 +15,31 @@ ustd::SerialConsole con;
 ustd::Net net(LED_BUILTIN);
 ustd::Mqtt mqtt;
 ustd::Ota ota;
+ustd::jsonfile jf;
 
-ustd::PressTempBMP180 bmp("BMP180-1", ustd::PressTempBMP180::FilterMode::FAST);
-ustd::IlluminanceLdr ldr("Ldr-1", A0);
-ustd::TempHumDHT dht("DHT-1", D5, 0);
 ustd::SensorDisplay display(128,64,0x3c);
+
+uint8_t current_slots;
 
 void setup() {
 #ifdef USE_SERIAL_DBG
     Serial.begin(115200);
+    Serial.println("Starting up...");
 #endif  // USE_SERIAL_DBG
-    display.begin("DHT-1/sensor/temperature", "hastates/sensor/balkon_temperature/state", "BMP180-1/sensor/pressureNN", "Studio C", "Balkon C", "Luftdruck hPa");
-    display.updateDisplay("init","init","init",0.0,0.0,0.0);
+    ustd::array<String> topics; 
+    ustd::array<String> captions;
+    ustd::array<String> msgs;
+    ustd::array<double> dirs={0,0,0,0};
+    for (uint8_t i=0; i<current_slots; i++) {
+        msgs[i]="init";
+        captions[i]="room";
+        topics[i]="some/topic";
+    }
+    jf.readStringArray("display/topics", topics);
+    jf.readStringArray("display/captions", captions);
+    current_slots=topics.length();
+    display.begin(current_slots, topics, captions);
+    display.updateDisplay(msgs,dirs);
 
     con.begin(&sched);
     net.begin(&sched);
@@ -36,35 +47,25 @@ void setup() {
     ota.begin(&sched);
     int tID = sched.add(appLoop, "main", 1000000);
 
-    // sensors start measuring pressure and temperature
-    bmp.setReferenceAltitude(518.0); // 518m above NN, now we also receive PressureNN values for sea level.
-    bmp.startRelativeAltitude(); // Use next pressureNN measurement as altitude reference
-    bmp.begin(&sched, ustd::PressTempBMP180::BMPSampleMode::ULTRA_HIGH_RESOLUTION);
-
-    ldr.begin(&sched);
-    dht.begin(&sched);
-
-    // subscribe to kernel's MQTT messages, the sensorUpdates() funktion does the event handling
-    // Interal BMP180 sensor
     auto fnall = [=](String topic, String msg, String originator) {
             display.sensorUpdates(topic, msg, originator);
         };
-    sched.subscribe(tID, "DHT-1/sensor/temperature", fnall);
-    sched.subscribe(tID, "BMP180-1/sensor/pressureNN", fnall);
-    // Get outside temp via homeassistant mqtt:
-    mqtt.addSubscription(tID,"hastates/sensor/balkon_temperature/state",fnall);
+    for (uint8_t i=0; i<current_slots; i++) {
+#ifdef USE_SERIAL_DBG
+        Serial.print("Subscribing: ");
+        Serial.println(topics[i]);
+#endif
+        mqtt.addSubscription(tID, topics[i], fnall);
+    }
+    // sched.subscribe(tID, "DHT-1/sensor/temperature", fnall);
 }
 
 void appLoop() {
     // If a sensors doesn't update values for 1/2 hr (1800sec), declare invalid.
-    if (time(nullptr)-display.lastUpdate_t1 > 1800) {
-        display.t1_val=0;
-    }
-    if (time(nullptr)-display.lastUpdate_t2 > 1800) {
-        display.t2_val=0;
-    }
-    if (time(nullptr)-display.lastUpdate_t3 > 1800) {
-        display.t3_val=0;
+    for (uint8_t i=0; i<current_slots; i++) {
+        if (time(nullptr)-display.lastUpdates[i] > 1800) {
+            display.vals_init[i]=false;
+        }
     }
 }
 

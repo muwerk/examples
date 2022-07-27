7cec99dd9c04099552e14169ef3ab0a543864998
Dominik Schlösser <dsc@dosc.net>
2022-07-27 12:52:05 +0200

    better encapsulated oled

--- pressureBmp/src/pressureBmp.cpp
+++ pressureBmp/src/pressureBmp.cpp
@@ -21,19 +21,18 @@ ustd::Ota ota;
 ustd::PressTempBMP180 bmp("BMP180-1", ustd::PressTempBMP180::FilterMode::FAST);
 ustd::IlluminanceLdr ldr("Ldr-1", A0);
 ustd::TempHumDHT dht("DHT-1", D5, 0);
-ustd::SensorDisplay display(128,64,0x3c);
+ustd::SensorDisplay display("display",128,64,0x3c);
 
 void setup() {
 #ifdef USE_SERIAL_DBG
     Serial.begin(115200);
 #endif  // USE_SERIAL_DBG
-    display.begin("DHT-1/sensor/temperature", "hastates/sensor/balkon_temperature/state", "BMP180-1/sensor/pressureNN", "Studio C", "Balkon C", "Luftdruck hPa");
-    display.updateDisplay("init","init","init",0.0,0.0,0.0);
-
     con.begin(&sched);
     net.begin(&sched);
     mqtt.begin(&sched);
     ota.begin(&sched);
+    display.begin(&sched, &mqtt);
+
     int tID = sched.add(appLoop, "main", 1000000);
 
     // sensors start measuring pressure and temperature
@@ -43,29 +42,9 @@ void setup() {
 
     ldr.begin(&sched);
     dht.begin(&sched);
-
-    // subscribe to kernel's MQTT messages, the sensorUpdates() funktion does the event handling
-    // Interal BMP180 sensor
-    auto fnall = [=](String topic, String msg, String originator) {
-            display.sensorUpdates(topic, msg, originator);
-        };
-    sched.subscribe(tID, "DHT-1/sensor/temperature", fnall);
-    sched.subscribe(tID, "BMP180-1/sensor/pressureNN", fnall);
-    // Get outside temp via homeassistant mqtt:
-    mqtt.addSubscription(tID,"hastates/sensor/balkon_temperature/state",fnall);
 }
 
 void appLoop() {
-    // If a sensors doesn't update values for 1/2 hr (1800sec), declare invalid.
-    if (time(nullptr)-display.lastUpdate_t1 > 1800) {
-        display.t1_val=0;
-    }
-    if (time(nullptr)-display.lastUpdate_t2 > 1800) {
-        display.t2_val=0;
-    }
-    if (time(nullptr)-display.lastUpdate_t3 > 1800) {
-        display.t3_val=0;
-    }
 }
 
 // Never add code to this loop, use appLoop() instead.
--- wirelessOledDisplay/src/wirelessOledDisplay.cpp
+++ wirelessOledDisplay/src/wirelessOledDisplay.cpp
@@ -15,99 +15,26 @@ ustd::SerialConsole con;
 ustd::Net net(LED_BUILTIN);
 ustd::Mqtt mqtt;
 ustd::Ota ota;
-ustd::jsonfile jf;
 
-ustd::SensorDisplay display(128,64,0x3c);
-
-uint8_t current_slots;
+ustd::SensorDisplay display("display", 128,64,0x3c);
 
 void setup() {
 #ifdef USE_SERIAL_DBG
     Serial.begin(115200);
     Serial.println("Starting up...");
 #endif  // USE_SERIAL_DBG
-    ustd::array<String> topics; 
-    ustd::array<String> captions;
-    ustd::array<String> msgs;
-    String layout, formats;
-    ustd::array<double> dirs={0,0,0,0};
-    for (uint8_t i=0; i<current_slots; i++) {
-        msgs[i]="init";
-        captions[i]="room";
-        topics[i]="some/topic";
-    }
-    layout=jf.readString("display/layout","SS|SS");
-    if (layout!="SS|SS" && layout!="L|SS" && layout!="L|L" && layout !="SS|L" && layout != "L" && layout != "SS") {
-        layout="SS|SS";
-#ifdef USE_SERIAL_DBG
-        Serial.print("Unsupported layout: ");
-        Serial.print(layout);
-        Serial.println(" please use (64x128 displays) 'SS|SS' or 'L|SS' or 'L|L' or 'SS|L' or (32x128 displays) 'L' or 'SS' ");
-#endif  // USE_SERIAL_DBG
-    }
-    formats=jf.readString("display/formats", "FFFF"); // four floats, either F (float), messages are converted to float fir %.1f, or S (string) messages displayed as-is.
-    while (formats.length()<4) {
-        formats += " ";
-    }
-    for (unsigned int i=0; i<formats.length(); i++) {
-        if (formats[i]!='F' && formats[i]!='S' && formats[i]!=' ') {
-            formats[i]='S';
-#ifdef USE_SERIAL_DBG
-            Serial.print("Unsupported formats string: ");
-            Serial.print(formats);
-            Serial.println(" should only contain 'F' for float, 'S' for string, or ' '.");
-#endif  // USE_SERIAL_DBG
-        }
-    }
-
-    jf.readStringArray("display/topics", topics);
-    jf.readStringArray("display/captions", captions);
-    current_slots=topics.length();
-    display.begin(current_slots, layout, formats, topics, captions);
-    display.updateDisplay(msgs,dirs);
 
     con.begin(&sched);
     net.begin(&sched);
     mqtt.begin(&sched);
     ota.begin(&sched);
-    int tID = sched.add(appLoop, "main", 1000000);
 
-    auto fnall = [=](String topic, String msg, String originator) {
-            display.sensorUpdates(topic, msg, originator);
-        };
-    for (uint8_t i=0; i<current_slots; i++) {
-#ifdef USE_SERIAL_DBG
-        Serial.print("Subscribing: ");
-        Serial.println(topics[i]);
-#endif
-        mqtt.addSubscription(tID, topics[i], fnall);
-    }
-    // sched.subscribe(tID, "DHT-1/sensor/temperature", fnall);
+    display.begin(&sched,&mqtt);
+
+    sched.add(appLoop, "main", 1000000);
 }
 
 void appLoop() {
-    const char *weekDays[]={"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"};
-    const char *pDay;
-    struct tm *plt;
-    time_t t;
-    char buf[64];
-    static char oldbuf[64]="";
-    // scruffy old c time functions 
-    t=time(nullptr);
-    plt=localtime(&t);
-    pDay=weekDays[plt->tm_wday];
-    // sprintf(buf,"%s %02d. %02d:%02d",pDay,plt->tm_mday, plt->tm_hour, plt->tm_min);
-    sprintf(buf,"%s %02d:%02d",pDay, plt->tm_hour, plt->tm_min);
-    if (strcmp(buf,oldbuf)) {
-        strcpy(oldbuf,buf);
-        sched.publish("clock/timeinfo", String(buf));
-    }
-    // If a sensors doesn't update values for 1 hr (3600sec), declare invalid.
-    for (uint8_t i=0; i<current_slots; i++) {
-        if (time(nullptr)-display.lastUpdates[i] > 3600) {
-            display.vals_init[i]=false;
-        }
-    }
 }
 
 // Never add code to this loop, use appLoop() instead.

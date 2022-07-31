a07417b29c7b086da735efb31f751805f61eb3a3
Dominik Schlösser <dsc@dosc.net>
2022-07-31 10:29:53 +0200

    Sample with both Oled and Tft display

---
+++ wirelessGfxPanel/src/wirelessGfxPanel.cpp
@@ -0,0 +1,45 @@
+#include "ustd_platform.h"
+#include "scheduler.h"
+#include "net.h"
+#include "mqtt.h"
+#include "ota.h"
+#include "jsonfile.h"
+
+#include "console.h"
+#include "mup_gfx_panel.h"
+
+void appLoop();
+
+ustd::Scheduler sched(10, 16, 32);
+ustd::SerialConsole con;
+ustd::Net net(LED_BUILTIN);
+ustd::Mqtt mqtt;
+ustd::Ota ota;
+
+ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 128, 128, D4, D3, (uint8_t)-1, "DE");
+ustd::GfxPanel displayOled("display2", ustd::GfxDrivers::DisplayType::SSD1306, 128,64, 0x3c, &Wire, "DE");
+
+void setup() {
+#ifdef USE_SERIAL_DBG
+    Serial.begin(115200);
+    Serial.println("Starting up...");
+#endif  // USE_SERIAL_DBG
+
+    con.begin(&sched);
+    net.begin(&sched);
+    mqtt.begin(&sched);
+    ota.begin(&sched);
+
+    displayTft.begin(&sched,&mqtt);
+    displayOled.begin(&sched,&mqtt);
+
+    sched.add(appLoop, "main", 1000000);
+}
+
+void appLoop() {
+}
+
+// Never add code to this loop, use appLoop() instead.
+void loop() {
+    sched.loop();
+}

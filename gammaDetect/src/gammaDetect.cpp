9c0aed6079e27ccf4d834eb1d9f9187a965ae6b4
Dominik Schlösser <dsc@dosc.net>
2022-08-03 18:51:34 +0200

    First gamma detector with GDK101

--- gammaDetect/src/gammaDetect.cpp
+++ gammaDetect/src/gammaDetect.cpp
@@ -8,7 +8,11 @@
 #include "console.h"
 #include "jsonfile.h"
 
+#include "doctor.h"
+#include "i2cdoctor.h"
+
 #include "mup_gfx_panel.h"
+#include "mup_gamma_gdk101.h"
 
 void appLoop();
 
@@ -17,8 +21,11 @@ ustd::SerialConsole con;
 ustd::Net net(LED_BUILTIN);
 ustd::Mqtt mqtt;
 ustd::Ota ota;
+ustd::I2CDoctor i2cdoc;
 
 ustd::GfxPanel display("display", ustd::GfxDrivers::DisplayType::ST7735, 128, 128, D4, D3, (uint8_t)-1, "DE");
+ustd::GammaGDK101 gammaG("GAMMA-1",ustd::GammaGDK101::FilterMode::FAST);
+
 // ustd::GfxPanel display("display", ustd::GfxDrivers::DisplayType::SSD1306, 128, 64, 0x3c);
 
 void setup() {
@@ -29,7 +36,9 @@ void setup() {
     net.begin(&sched);
     mqtt.begin(&sched);
     ota.begin(&sched);
+    i2cdoc.begin(&sched, &Wire);
     display.begin(&sched, &mqtt);
+    gammaG.begin(&sched);
 
     int tID = sched.add(appLoop, "main", 1000000);
 

cd95998c8ccf32ec7bf24dfb93f4ff2f1eb37045
Dominik Schlösser <dsc@dosc.net>
2022-08-04 11:00:42 +0200

    ESP32S2 tests

--- wirelessGfxPanel/src/wirelessGfxPanel.cpp
+++ wirelessGfxPanel/src/wirelessGfxPanel.cpp
@@ -18,8 +18,10 @@ ustd::Ota ota;
 
 // make sure that two json files exist: display1.json and display2.json, corresponding to the names of the GfxPanel s.
 #ifdef __ESP32__
-ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 128, 128, 4, 3, (uint8_t)-1, "DE");
+//                                                                                     CS  DC  RST
+ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 160, 128, 34, 33,  32, "DE");
 #else
+//                                                                                     CS  DC  RST
 ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 128, 128, D4, D3, (uint8_t)-1, "DE");
 #endif
 ustd::GfxPanel displayOled("display2", ustd::GfxDrivers::DisplayType::SSD1306, 128,64, 0x3c, &Wire, "DE");
@@ -35,8 +37,10 @@ void setup() {
     mqtt.begin(&sched);
     ota.begin(&sched);
 
-    displayTft.begin(&sched,&mqtt);
-    displayOled.begin(&sched,&mqtt);
+Serial.println("Starting display");
+//    displayTft.begin(&sched,&mqtt);
+Serial.println("Display started");
+//    displayOled.begin(&sched,&mqtt);
 
     sched.add(appLoop, "main", 1000000);
 }

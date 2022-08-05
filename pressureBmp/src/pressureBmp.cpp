6b4535fc1748c914b8bd92a91c6e9f87e4770328
Dominik Schlösser <dsc@dosc.net>
2022-08-05 08:41:09 +0200

    complete example

--- pressureBmp/src/pressureBmp.cpp
+++ pressureBmp/src/pressureBmp.cpp
@@ -7,8 +7,6 @@
 #include "console.h"
 #include "mup_presstemp_bmp180.h"
 #include "mup_presstemphum_bme280.h"
-//#include "mup_illuminance_ldr.h"
-//#include "mup_temphum_dht.h"
 #include "mup_gfx_panel.h"
 
 void appLoop();
@@ -23,13 +21,8 @@ ustd::Ota ota;
 ustd::PressTempBMP180 bmp1("BMP180-1", ustd::PressTempBMP180::FilterMode::FAST);
 ustd::PressTempHumBME280 bme1("BME280-1", ustd::PressTempHumBME280::FilterMode::FAST, 0x76);
 
-//ustd::IlluminanceLdr ldr("Ldr-1", A0);
-//#ifdef __ESP32__
-//ustd::TempHumDHT dht("DHT-1", 5, 0);
-//#else
-//ustd::TempHumDHT dht("DHT-1", D5, 0);
-//#endif
-ustd::GfxPanel display("display", ustd::GfxDrivers::DisplayType::SSD1306, 128, 64, 0x3c);
+ustd::GfxPanel displayOled("display_oled", ustd::GfxDrivers::DisplayType::SSD1306, 128, 64, 0x3c);
+ustd::GfxPanel displayTft("display_tft", ustd::GfxDrivers::DisplayType::ST7735, 128, 128, D4, D3, -1);
 
 void setup() {
 #ifdef USE_SERIAL_DBG
@@ -39,7 +32,8 @@ void setup() {
     net.begin(&sched);
     mqtt.begin(&sched);
     ota.begin(&sched);
-    display.begin(&sched, &mqtt);
+    displayOled.begin(&sched, &mqtt);
+    displayTft.begin(&sched, &mqtt);
 
     int tID = sched.add(appLoop, "main", 1000000);
 
@@ -50,9 +44,6 @@ void setup() {
     bme1.startRelativeAltitude(); // Use next pressureNN measurement as altitude reference
     bmp1.begin(&sched, ustd::PressTempBMP180::BMPSampleMode::ULTRA_HIGH_RESOLUTION);
     bme1.begin(&sched, ustd::PressTempHumBME280::BMESampleMode::ULTRA_HIGH_RESOLUTION);
-
-    //ldr.begin(&sched);
-    //dht.begin(&sched);
 }
 
 void appLoop() {

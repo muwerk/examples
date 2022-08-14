39be87d1685e032b7a974218edff2229a0539b73
Dominik Schlösser <dsc@dosc.net>
2022-08-14 17:09:01 +0200

    Altitude real time graphs

--- pressureBmp/src/pressureBmp.cpp
+++ pressureBmp/src/pressureBmp.cpp
@@ -1,3 +1,9 @@
+
+#define USE_OLED
+#define USE_TFT
+#define USE_BMP180
+#define USE_BME280
+
 #include "ustd_platform.h"
 #include "scheduler.h"
 #include "net.h"
@@ -5,9 +11,14 @@
 #include "ota.h"
 
 #include "console.h"
+#ifdef USE_BMP180
 #include "mup_presstemp_bmp180.h"
+#endif
+#ifdef USE_BME280
 #include "mup_presstemphum_bme280.h"
+#endif
 #include "mup_gfx_panel.h"
+
 
 void appLoop();
 
@@ -17,18 +28,27 @@ ustd::Net net(LED_BUILTIN);
 ustd::Mqtt mqtt;
 ustd::Ota ota;
 
-
+#ifdef USE_BMP180
 ustd::PressTempBMP180 bmp1("BMP180-1", ustd::PressTempBMP180::FilterMode::FAST);
-ustd::PressTempHumBME280 bme1("BME280-1", ustd::PressTempHumBME280::FilterMode::FAST, 0x76);
-
-ustd::GfxPanel displayOled("display_oled", ustd::GfxDrivers::DisplayType::SSD1306, 128, 64, 0x3c);
-#ifdef __ESP32__
-//                                                                                     CS  DC  RST
-ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 160, 128,  5, 16, 17, "DE");
-
-#else
-ustd::GfxPanel displayTft("display_tft", ustd::GfxDrivers::DisplayType::ST7735, 128, 128, D4, D3, -1);
 #endif
+#ifdef USE_BME280
+ustd::PressTempHumBME280 bme1("BME280-1", ustd::PressTempHumBME280::FilterMode::FAST, 0x76);
+#endif
+
+#ifdef USE_OLED
+ustd::GfxPanel displayOled("display_oled", ustd::GfxDrivers::DisplayType::SSD1306, 128,64, 0x3c, &Wire, "DE");
+#endif // USE_OLED
+
+#ifdef USE_TFT
+#ifdef __ESP32__
+#define USE_CANVAS_MEMORY true
+// pinout                                                                              CS  DC  RST
+ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 160, 128,  5, 16, 17, "DE");
+#else // ESP8266
+#define USE_CANVAS_MEMORY false
+ustd::GfxPanel displayTft("display_tft", ustd::GfxDrivers::DisplayType::ST7735, 128, 128, D4, D3, (uint8_t)-1, "DE");
+#endif // __ESP32__
+#endif // USE_TFT
 
 void setup() {
 #ifdef USE_SERIAL_DBG
@@ -38,18 +58,35 @@ void setup() {
     net.begin(&sched);
     mqtt.begin(&sched);
     ota.begin(&sched);
-    displayOled.begin(&sched, &mqtt);
-    displayTft.begin(&sched, &mqtt,false);
+#ifdef USE_OLED
+    const char *topics1[]={"BMP180-1/sensor/temperature","BMP180-1/sensor/temperature",
+                           "BMP180-1/sensor/pressureNN", "BME280-1/sensor/relativealtitude"};
+    const char *captions1[]={"BP18 _C", "BP18 _C", "BP18 _hPa", "BE28 _m"};
+    displayOled.begin(&sched, &mqtt,"fg|ig",4,topics1,captions1);
+    displayOled.setSlotHistorySampleRateMs(3,2000);   // show altitude changes every 50ms
+#endif
+#ifdef USE_TFT
+    const char *topics2[]={"BME280-1/sensor/temperature", "BME280-1/sensor/temperature", "BME280-1/sensor/humidity",
+                           "BME280-1/sensor/humidity", "BME280-1/sensor/pressureNN", "BME280-1/sensor/pressureNN", 
+                           "BME280-1/sensor/relativealtitude"};
+    const char *captions2[]={"BE28 _C", "BE28 _C", "BE28 _%rH","BE28 _%rH", "BE28 _hPa", "BE28 _hPa", "BE28 _m"};
+    displayTft.begin(&sched, &mqtt,"fg|fg|ig|G",7,topics2,captions2,USE_CANVAS_MEMORY);  // Only use canvas memory if ESP32 (large memory requirement!)  
+    displayTft.setSlotHistorySampleRateMs(6,200);   // show altitude changes every n ms
+    bme1.setPollRateMs(200);
+#endif
 
     int tID = sched.add(appLoop, "main", 1000000);
-
     // sensors start measuring pressure and temperature
+#ifdef USE_BMP180
     bmp1.setReferenceAltitude(518.0); // 518m above NN, now we also receive PressureNN values for sea level.
-    bme1.setReferenceAltitude(518.0); // 518m above NN, now we also receive PressureNN values for sea level.
     bmp1.startRelativeAltitude(); // Use next pressureNN measurement as altitude reference
-    bme1.startRelativeAltitude(); // Use next pressureNN measurement as altitude reference
     bmp1.begin(&sched, ustd::PressTempBMP180::BMPSampleMode::ULTRA_HIGH_RESOLUTION);
-    bme1.begin(&sched, ustd::PressTempHumBME280::BMESampleMode::ULTRA_HIGH_RESOLUTION);
+#endif
+#ifdef USE_BME280
+    bme1.setReferenceAltitude(518.0); // 518m above NN, now we also receive PressureNN values for sea level.
+    bme1.startRelativeAltitude(); // Use next pressureNN measurement as altitude reference
+    bme1.begin(&sched, ustd::PressTempHumBME280::BMESampleMode::STANDARD);
+#endif
 }
 
 void appLoop() {

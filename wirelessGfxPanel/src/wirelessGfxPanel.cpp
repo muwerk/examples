19123b1f626f240a2e53d90d0d25c5cdfaa67079
Dominik Schlösser <dsc@dosc.net>
2022-08-15 18:33:45 +0200

    formatting

--- wirelessGfxPanel/src/wirelessGfxPanel.cpp
+++ wirelessGfxPanel/src/wirelessGfxPanel.cpp
@@ -13,7 +13,7 @@ void appLoop();
 ustd::Scheduler sched(10, 16, 32);
 ustd::SerialConsole con;
 #ifdef __ESP32S2__
-ustd::Net net(2); // ESP32S2 forgot about LED_BUILTIN...
+ustd::Net net(2);  // ESP32S2 forgot about LED_BUILTIN...
 #else
 ustd::Net net(LED_BUILTIN);
 #endif
@@ -24,15 +24,15 @@ ustd::Ota ota;
 #ifdef __ESP32__
 #ifdef __ESP32S2__
 //                                                                                     CS  DC  RST
-ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 160, 128,  5, 16, 17, "DE");
+ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 160, 128, 5, 16, 17, "DE");
 #else
 //                                                                                     CS  DC  RST
-ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 160, 128,  5, 16, 17, "DE");
-#endif // __ESP32S2__
+ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 160, 128, 5, 16, 17, "DE");
+#endif  // __ESP32S2__
 #else
 //                                                                                     CS  DC  RST
-//ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 128, 128, D4, D3, (uint8_t)-1, "DE");
-ustd::GfxPanel displayOled("display2", ustd::GfxDrivers::DisplayType::SSD1306, 128,64, 0x3c, &Wire, "DE");
+// ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7735, 128, 128, D4, D3, (uint8_t)-1, "DE");
+ustd::GfxPanel displayOled("display2", ustd::GfxDrivers::DisplayType::SSD1306, 128, 64, 0x3c, &Wire, "DE");
 #endif
 
 void setup() {
@@ -47,13 +47,13 @@ void setup() {
     ota.begin(&sched);
 
 #ifdef __ESP32__
-Serial.println("Starting display");
-    displayTft.begin(&sched,&mqtt);
-Serial.println("Display started");
+    Serial.println("Starting display");
+    displayTft.begin(&sched, &mqtt);
+    Serial.println("Display started");
 #else
-    const char *topics[]={"clock/timeinfo", "!hastates/sensor/klima_nordseite_temperature/state", "!hastates/sensor/klima_kuche_temperature/state"};
-    const char *captions[]={"Time", "Nord _C", "Kueche _C"};
-    displayOled.begin(&sched,&mqtt,"S|ff",3,topics,captions);
+    const char *topics[] = {"clock/timeinfo", "!hastates/sensor/klima_nordseite_temperature/state", "!hastates/sensor/klima_kuche_temperature/state"};
+    const char *captions[] = {"Time", "Nord _C", "Kueche _C"};
+    displayOled.begin(&sched, &mqtt, "S|ff", 3, topics, captions);
 #endif
     sched.add(appLoop, "main", 1000000);
 }

e3be1a92d1fcb157ca2001a0d160b9f1dff0099b
Dominik Schlösser <dsc@dosc.net>
2023-01-01 16:21:41 +0100

    Frame

---
+++ randomNumbers/README.md
@@ -0,0 +1,6 @@
+Random number generator
+=======================
+
+TBD.
+
+<img src="https://github.com/muwerk/examples/blob/master/Resources/RandomNumbers.jpg" width="50%">
---
+++ randomNumbers/src/randomNumbers.cpp
@@ -0,0 +1,41 @@
+#define USE_SERIAL_DBG 1
+
+#include "scheduler.h"
+#include "net.h"
+#include "mqtt.h"
+#include "ota.h"
+
+#include "Wire.h"
+#include <SPI.h>
+
+#include "mup_rng.h"
+
+ustd::Rng rng("rng", D2, 0);
+
+void appLoop();
+
+ustd::Scheduler sched(10, 16, 32);
+ustd::Net net(LED_BUILTIN);
+ustd::Mqtt mqtt;
+ustd::Ota ota;
+
+void setup() {
+#ifdef USE_SERIAL_DBG
+    Serial.begin(115200);
+    Serial.println("Startup");
+#endif  // USE_SERIAL_DBG
+    net.begin(&sched);
+    mqtt.begin(&sched);
+    ota.begin(&sched);
+    int tID = sched.add(appLoop, "main", 1000000);
+
+    rng.begin(&sched);
+}
+
+void appLoop() {
+}
+
+// Never add code to this loop, use appLoop() instead.
+void loop() {
+    sched.loop();
+}

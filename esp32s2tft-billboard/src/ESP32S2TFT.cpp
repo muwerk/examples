58d7758705782188d010a8cffc03fb1ecade27bf
Dominik Schlösser <dsc@dosc.net>
2023-06-22 17:16:06 +0200

    Stub

---
+++ esp32s2tft-billboard/src/ESP32S2TFT.cpp
@@ -0,0 +1,54 @@
+#include "ustd_platform.h"
+#include "scheduler.h"
+#include "net.h"
+#include "indrajala.h"
+#include "ota.h"
+#include "jsonfile.h"
+
+#include "console.h"
+#define OPTION_NO_MQTT
+#include "mup_gfx_panel.h"
+
+void appLoop();
+
+ustd::Scheduler sched(10, 16, 32);
+ustd::SerialConsole con;
+ustd::Net net(LED_BUILTIN);
+ustd::Indrajala indra;
+ustd::Ota ota;
+
+ustd::GfxPanel displayTft("display1", ustd::GfxDrivers::DisplayType::ST7789, 240, 135, TFT_CS, TFT_DC, TFT_RST, "DE");
+
+void setup() {
+#ifdef USE_SERIAL_DBG
+    Serial.begin(115200);
+    Serial.println("Starting up...");
+#endif  // USE_SERIAL_DBG
+
+    con.begin(&sched);
+    net.begin(&sched);
+    indra.begin(&sched);
+    ota.begin(&sched);
+
+    Serial.println("Starting display");
+    // turn on backlite
+    pinMode(TFT_BACKLITE, OUTPUT);
+    digitalWrite(TFT_BACKLITE, HIGH);
+
+    // turn on the TFT / I2C power supply
+    pinMode(TFT_I2C_POWER, OUTPUT);
+    digitalWrite(TFT_I2C_POWER, HIGH);
+    delay(10);
+    displayTft.begin(&sched, false);
+    Serial.println("Display started");
+
+    sched.add(appLoop, "main", 1000000);
+}
+
+void appLoop() {
+}
+
+// Never add code to this loop, use appLoop() instead.
+void loop() {
+    sched.loop();
+}

7fdd63beae367ca82020dc625b23e2b950e8279f
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-09-20 18:37:55 +0200

    Magnetic field tests

--- earthState/src/earthState.cpp
+++ earthState/src/earthState.cpp
@@ -9,8 +9,15 @@
 #include <SPI.h>
 
 #include "mup_rng.h"
+#include "mup_magnetic_hmc5883l.h"
 
-ustd::Rng rng("rng", 17, 2, ustd::Rng::IM_CHANGE, 16, false, 25600);
+// Send mqtt message with topic <esp-hostname>/RNG-1/rng/data/get with empty message body to receive a block of hex random data
+// Send mqtt message with topic <esp-hostname>/RNG-1/rng/state/get with empty message body to receive the state of the RNG
+// <esp-hostname> is configured in data/net.json, in the field "hostname". This also contains the wifi credentials.
+// The MQTT server is configured in data/mqtt.json, in the field "host".
+// Copy data/net-default.json and data/mqtt-default.json to data/net.json and data/mqtt.json and adjust the fields to your needs.
+// ustd::Rng rng("RNG-1", RNG_INPUT_PIN, 2, ustd::Rng::IM_CHANGE, RNG_STATE_LED_PIN, false, 25600);
+ustd::MagneticFieldHMC5883L magField("MAG-1");
 
 void appLoop();
 
@@ -22,6 +29,8 @@ ustd::Ota ota;
 void setup() {
 #ifdef USE_SERIAL_DBG
     Serial.begin(115200);
+    while (!Serial)
+        ;  // Wait for Serial to be ready
     Serial.println("Startup");
 #endif  // USE_SERIAL_DBG
     net.begin(&sched);
@@ -29,17 +38,19 @@ void setup() {
     ota.begin(&sched);
     int tID = sched.add(appLoop, "main", 1000000);
 
-    rng.begin(&sched, 100000);
+    // rng.begin(&sched, 100000);
+    Serial.println("Starting magnetic field sensor");
+    magField.begin(&sched, &Wire);
 }
 
 unsigned long oldSamples = 0;
 void appLoop() {
 #ifdef USE_SERIAL_DBG
-    if (rng.getSampleCount() - oldSamples > 100000) {
-        Serial.print(rng.getSampleCount());
-        Serial.print(" ");
-        oldSamples = rng.getSampleCount();
-    }
+    // if (rng.getSampleCount() - oldSamples > 100000) {
+    //     Serial.print(rng.getSampleCount());
+    //     Serial.print(" ");
+    //     oldSamples = rng.getSampleCount();
+    // }
 #endif  // USE_SERIAL_DBG
 }
 
--- earthState/ull.sh
+++ earthState/ull.sh
@@ -6,6 +6,7 @@ for f in .pio/libdeps/*; do
         cp -v ../../muwerk/*.h $f/muwerk/
         cp -v ../../munet/*.h $f/munet/
         cp -av ../../mupplet-core/src $f/mupplet-core
+        cp -av ../../mupplet-sensor/src $f/mupplet-sensor
         cp -av ../../mupplet-display/src $f/mupplet-display
     fi
 done
